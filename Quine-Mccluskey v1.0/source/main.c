e379e621257b7bfbb397f268c402bfb876745e0e
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-10 22:19:42 +0530

    Update main.c

--- Quine-Mccluskey v1.0/source/main.c
+++ Quine-Mccluskey v1.0/source/main.c
@@ -18,108 +18,104 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 */
-
-// bugs found : after entering minterms input, instead of ending it with -1 but with *1, all the leftover space of array is filled with 0
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 #include "quine.h"
 #include "helper.h"
-					
-int main(){
-	
-	#ifdef _WIN32
+
+int main() {
+    #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
-	#endif
-	
-	int var , min_terms[10000] , min_count = 0, dont_care_count = 0 , temp;
-	
-	// data input start
-	printf("Enter no. of variables : ");
-	scanf("%d" ,&var);
-	
-	printf("Enter min terms (-1 to end) : ");
-	while(min_count < pow(2,var)){
-		scanf("%d",&temp);
-		if(temp == -1) break;
-		min_terms[min_count++] = temp;
-	}
-	
-	printf("Enter dont care (-1 to end) : ");
-	while((min_count+dont_care_count) < pow(2,var)){
-		scanf("%d",&temp);
-		if(temp == -1) break;
-		min_terms[min_count + dont_care_count] = temp;
-		dont_care_count++;
-	}
-	
-	int n_terms = min_count + dont_care_count;
-	char binary[1000][100];
-	ToBinary(binary ,min_terms, n_terms ,var);
-	
-	printf("\n\n%d Mid terms : ",min_count); 
-	for(int i=0;i<min_count;i++) 
-		printf("%d ",min_terms[i]);
-	
-	printf("\n%d Dont care  : ",dont_care_count); 
-	for(int i = min_count; i < n_terms; i++) 
-		printf("%d ",min_terms[i]);
-		
-	//data input stops
-	
-	static quine group[100], reduced[100] , prime;  // stack memory very low (8mb). group[100] -> 30mb
-	
-	fill_group_table(group, min_terms, binary, n_terms, var);
-	
-	int CanReduce = reduce_table(group, reduced, var);
-	prime_implicants(group, &prime, var); // get prime-implicant from the Uncombineds
-	printf("\n\nInitial Grouping:\n");
+    #endif
+    
+    int var, min_terms[10000], min_count = 0, dont_care_count = 0, temp;
+    
+    // Data input
+    printf("Enter no. of variables: ");
+    scanf("%d", &var);
+    
+    printf("Enter min terms (-1 to end): ");
+    while (min_count < pow(2, var)) {
+        scanf("%d", &temp);
+        if (temp == -1) break;
+        min_terms[min_count++] = temp;
+    }
+    
+    printf("Enter dont care (-1 to end): ");
+    while ((min_count + dont_care_count) < pow(2, var)) {
+        scanf("%d", &temp);
+        if (temp == -1) break;
+        min_terms[min_count + dont_care_count] = temp;
+        dont_care_count++;
+    }
+    
+    int n_terms = min_count + dont_care_count;
+    char binary[1000][100];
+    ToBinary(binary, min_terms, n_terms, var);
+    
+    printf("\n\n%d Min terms: ", min_count); 
+    for (int i = 0; i < min_count; i++) 
+        printf("%d ", min_terms[i]);
+    
+    printf("\n%d Dont care: ", dont_care_count); 
+    for (int i = min_count; i < n_terms; i++) 
+        printf("%d ", min_terms[i]);
+    
+    // Initialize data structures
+    static quine group[100], reduced[100], prime;
+    
+    fill_group_table(group, min_terms, binary, n_terms, var);
+    
+    int CanReduce = reduce_table(group, reduced, var);
+    prime_implicants(group, &prime, var);
+    
+    printf("\n\nInitial Grouping:\n");
     displayGroups(group, var);
-	
-	
-	int i = 1;
-    while(CanReduce){
-		for (int i = 0; i <= var; i++) group[i] = reduced[i];
+    
+    int i = 1;
+    while (CanReduce) {
+        for (int i = 0; i <= var; i++) group[i] = reduced[i];
         CanReduce = reduce_table(group, reduced, var);
-		prime_implicants(group, &prime, var);
-		
-		printf("\n%dth Reduction: \n" , i++);
+        prime_implicants(group, &prime, var);
+        
+        printf("\n%dth Reduction: \n", i++);
         displayGroups(group, var);	
     }	
-	
-	printf("\n");
-	display_implicants(&prime);
-	
-	char result[1000][100];
-	char essential_table[100][100][6];
-	
-	int iterate = essential_implicants(&prime, essential_table, min_terms, min_count, result);
-	
-	printf("\n\n\nTable to find Essential prime Implicants: \n");
-	display_essential_table(&prime, essential_table, min_terms, min_count);
-	
-	printf("\n\nEssential Prime Implicants : ");
-	for(int i = 0; i < iterate; i++)
-		printf("%s  ", result[i]);
-	printf("\n");
-	
-	printf("\nresult : Y (");
-	for(int i = 0; i < var; i++){
-		printf("%c" , (char)('A'+i));
-		if(i < var-1) printf(",");
-		else printf(") = ");
-	}
-	
-	for(int i = 0; i < iterate; i++){
-		Expression(result[i]);
-		if(i < iterate-1) printf(" + ");
-	}
-	printf("\n\n");
-	return 0;
+    
+    printf("\n");
+    display_implicants(&prime);
+    
+    char result[1000][100];
+    char essential_table[100][100][6];
+    
+    int iterate = essential_implicants(&prime, essential_table, min_terms, min_count, result);
+    
+    printf("\n\n\nTable to find Essential prime Implicants: \n");
+    display_essential_table(&prime, essential_table, min_terms, min_count);
+    
+    printf("\n\nEssential Prime Implicants: ");
+    for (int i = 0; i < iterate; i++)
+        printf("%s  ", result[i]);
+    printf("\n");
+    
+    printf("\nResult: Y (");
+    for (int i = 0; i < var; i++) {
+        printf("%c", (char)('A' + i));
+        if (i < var - 1) printf(",");
+        else printf(") = ");
+    }
+    
+    for (int i = 0; i < iterate; i++) {
+        Expression(result[i]);
+        if (i < iterate - 1) printf(" + ");
+    }
+    printf("\n\n");
+    
+    return 0;
 }
