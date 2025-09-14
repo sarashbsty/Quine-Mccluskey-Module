107516a132fa471675b37611db9131b25ad1ef2f
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-15 00:36:41 +0530

    Refactor display_essential_table for clarity and correctness
    Renamed the min_terms parameter to Minterms for consistency. Improved table row and border rendering logic for better output formatting and readability.

--- Quine-Mccluskey v1.0/source/display-essential-table.c
+++ Quine-Mccluskey v1.0/source/display-essential-table.c
@@ -4,7 +4,7 @@
 #include "quine.h" // quine struture
 
 //check for improvemnt
-void display_essential_table(const quine *prime , char arr[100][100][6] , int min_terms[] , int min_count){
+void display_essential_table(const quine *prime , char arr[100][100][6] , int Minterms[] , int min_count){
 	
 	int n = 1+4+3+(5*min_count)+2;
     char line1[1000] , line2[2][500];
@@ -13,22 +13,24 @@ void display_essential_table(const quine *prime , char arr[100][100][6] , int mi
 	make_line(line2[0] , "─" , 3+2 , 3);
 	make_line(line2[1] , "─" , n-7-1, 3);
 	
-	printf("╔%s╗\n",line1);
-	printf("│        ");
-	for(int i=0;i<min_count;i++) 
-		printf("%-5d" , min_terms[i]);
-	printf("│\n");
-	printf("├%s┬%s┤\n",line2[0],line2[1]);
+	int row = prime->count;
 	
-	for(int i = 0; i < prime->count; i++){
-		printf("│ [%c] │ " , (char)('A'+i));
-		for(int x = 0; x < min_count; x++)
-			printf("%-5s" , arr[i][min_terms[x]]);
-		printf(" │\n");
+	printf("╔%s╗\n",line1);
+	
+	printf("│        ");
+	for(int j = 0; j < min_count; j++) 
+		printf("%-5d" , Minterms[j]);
+	printf("│\n");
+	
+	for(int i = 0; i < row; i++){
 		
-		if(i == prime->count-1) 
-			printf("╚%s┴%s╝\n",line2[0],line2[1]);
-		else
-			printf("├%s┼%s┤\n",line2[0],line2[1]);
+		if(i == 0) printf("├%s┬%s┤\n",line2[0],line2[1]);
+		else printf("├%s┼%s┤\n",line2[0],line2[1]);
+		
+		printf("│ [%c] │ " , (char)('A'+i));
+		for(int j = 0; j < min_count; j++)
+			printf("%-5s" , arr[i][Minterms[j]]);
+		printf(" │\n");
 	}
+	printf("╚%s┴%s╝\n",line2[0],line2[1]);
 }
