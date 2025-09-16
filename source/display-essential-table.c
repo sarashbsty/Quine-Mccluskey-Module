0a250b1f34bf28e119a45c6b83ee53d63394a47f
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-16 22:37:28 +0530

    Improve table column alignment in display_essential_table
    Adjusted the width and formatting of the first column in the display_essential_table function for better alignment and consistency. This change ensures that the table headers and row labels are properly aligned regardless of the min_count value.

--- source/display-essential-table.c
+++ source/display-essential-table.c
@@ -5,7 +5,7 @@
 
 void display_essential_table(const quine *prime , char arr[100][100][6] , int Minterms[] , int min_count){
 	
-	int width[2] = { 3 , 5*min_count };
+	int width[2] = { 1 , 5*min_count };
 	int n = 2 + width[0] + 3 + width[1] + 2;
 	
     char line1[1000] , line2[2][500];
@@ -15,7 +15,7 @@ void display_essential_table(const quine *prime , char arr[100][100][6] , int Mi
 		
 	printf("╔%s╗\n",line1);
 	
-	printf("│        ");
+	printf("│ %-*s    " , width[0] , " ");  // this is made just to match with the lower widths
 	for(int j = 0; j < min_count; j++) 
 		printf("%-5d" , Minterms[j]);
 	printf("│\n");
@@ -25,7 +25,7 @@ void display_essential_table(const quine *prime , char arr[100][100][6] , int Mi
 		if(i == 0) printf("├%s┬%s┤\n",line2[0],line2[1]);
 		else printf("├%s┼%s┤\n",line2[0],line2[1]);
 		
-		printf("│ [%c] │ " , (char)('A'+i));
+		printf("│ %-*c │ " , width[0] , (char)('A'+i));
 		for(int j = 0; j < min_count; j++)
 			printf("%-5s" , arr[i][Minterms[j]]);
 		printf(" │\n");
