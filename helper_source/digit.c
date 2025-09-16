56ddb5b3442111b5444d5fdc74847c5208762ead
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-17 01:10:27 +0530

    added digit function
    To find the number of digits of a number and declared at helper header

---
+++ helper_source/digit.c
@@ -0,0 +1,8 @@
+int digit(int x){
+	int count = 0;
+	while(x > 0){
+		count++;
+		x = x/10;
+	}
+	return count;
+}
--- include/helper.h
+++ include/helper.h
@@ -7,6 +7,7 @@ void Expression(char binary[]);
 int is_exist(char arr[][100], const char item[] , int);
 int get_minterms(int min_terms[] , int start , int max_terms);
 void make_line(char str[] , const char* c , int n , int byte);
+int digit(int x);
 
 static inline void clear_input_buffer(){
 	int c;
