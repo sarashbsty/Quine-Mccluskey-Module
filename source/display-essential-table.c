1a2451bbef8be834d2b4ed1af8751043fb70fd4e
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-16 23:20:09 +0530

    Improve essential table display formatting
    Enhanced the display_essential_table function to add a new column for binary representation, dynamically calculate column widths, and update table borders and headers for better readability.

--- source/display-essential-table.c
+++ source/display-essential-table.c
@@ -5,30 +5,30 @@
 
 void display_essential_table(const quine *prime , char arr[100][100][6] , int Minterms[] , int min_count){
 	
-	int width[2] = { 1 , 5*min_count };
-	int n = 2 + width[0] + 3 + width[1] + 2;
+	int width[3] = { 1 , strlen(prime->binary[0])+1 ,5*min_count };
+	int n = 2 + width[0] + 3 + width[1] + 3 + width[2] + 2;
 	
-    char line1[1000] , line2[2][500];
+    char line1[1000] , line2[3][500];
 	make_line(line1 , "─" , n-2 , 3);
-	make_line(line2[0] , "─" , width[0]+2 , 3);
-	make_line(line2[1] , "─" , width[1]+2 , 3);
+	for(int i = 0; i < 3; i++)
+		make_line(line2[i] ,"─", width[i]+2 ,3);
 		
 	printf("╔%s╗\n",line1);
 	
-	printf("│ %-*s    " , width[0] , " ");  // this is made just to match with the lower widths
+	printf("│ %-*s   %-*s    " , width[0] , " " , width[1] , " ");  // this is made just to match with the lower widths
 	for(int j = 0; j < min_count; j++) 
 		printf("%-5d" , Minterms[j]);
 	printf("│\n");
 	
 	for(int i = 0; i < prime->count; i++){
 		
-		if(i == 0) printf("├%s┬%s┤\n",line2[0],line2[1]);
-		else printf("├%s┼%s┤\n",line2[0],line2[1]);
+		if(i == 0) printf("├%s┬%s┬%s┤\n",line2[0],line2[1],line2[2]);
+		else printf("├%s┼%s┼%s┤\n",line2[0],line2[1],line2[2]);
 		
-		printf("│ %-*c │ " , width[0] , (char)('A'+i));
+		printf("│ %-*c │ %-*s │ " , width[0] , (char)('A'+i) , width[1] , prime->binary[i]);
 		for(int j = 0; j < min_count; j++)
 			printf("%-5s" , arr[i][Minterms[j]]);
 		printf(" │\n");
 	}
-	printf("╚%s┴%s╝\n",line2[0],line2[1]);
+	printf("╚%s┴%s┴%s╝\n",line2[0],line2[1],line2[2]);
 }
