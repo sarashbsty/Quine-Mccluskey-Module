ce2f67503ecf4a9553a39894efc9c184698d4a0b
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-18 09:29:57 +0530

    Update ToBinary.c

--- helper_source/ToBinary.c
+++ helper_source/ToBinary.c
@@ -1,10 +1,9 @@
 #include "helper.h"
 
-void ToBinary(char binary[][500] , int minterms[] , int count, int var){
-	for(int i = 0; i < count; i++){
-		int j;
-		for(j = 0; j < var; j++)
+void ToBinary(char binary[][500] , int minterms[] , int n_terms, int var){
+	for(int i = 0; i < n_terms; i++){
+		for(int j = 0; j < var; j++)
 			binary[i][var-1-j] = ((minterms[i] >> j) & 1) ? '1' : '0';
-		binary[i][j] = '\0';
+		binary[i][var] = '\0';
 	}
 }	
