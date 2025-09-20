e79ef25a287db424d82bcd94d667b140f658d0b7
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-20 11:27:58 +0530

    Refactor display_essential_table column layout
    Simplifies the table structure by reducing the number of columns from three to two, updating width calculations, and adjusting table border formatting accordingly. This makes the table output more concise and easier to read.

--- source/display-essential-table.c
+++ source/display-essential-table.c
@@ -6,18 +6,18 @@
 
 void display_essential_table(const quine *prime , char arr[][100][6] , int Minterms[] , int min_count){
 	
-	int len = strlen(prime->binary[0]);  
-	int width[3] = { (len<10) ? 10 : (len*2)+1  , strlen(prime->binary[0])+1 ,5*min_count };
-	int n = 2 + width[0] + 3 + width[1] + 3 + width[2] + 2;
+	int n = strlen(prime->binary[0]);  
+	int width[2] = {(n<10) ? 10 : (n*2)+1 , 5*min_count };
+	int total_len = 2 + width[0] + 3 + width[1] + 2;
 	
     char line1[1000] , line2[3][500];
-	make_line(line1 , "─" , n-2 , 3);
-	for(int i = 0; i < 3; i++)
+	make_line(line1 , "─" , total_len-2 , 3);
+	for(int i = 0; i < 2; i++)
 		make_line(line2[i] ,"─", width[i]+2 ,3);
 		
 	printf("╔%s╗\n",line1);
 	
-	printf("│ %-*s   %-*s    " , width[0] , " " , width[1] , " ");  // this is made just to match with the lower widths
+	printf("│ %-*s    " , width[0] , " ");  // this is made just to match with the lower widths
 	for(int j = 0; j < min_count; j++) 
 		printf("%-5d" , Minterms[j]);
 	printf("│\n");
@@ -31,15 +31,15 @@ void display_essential_table(const quine *prime , char arr[][100][6] , int Minte
 		strcpy(exp , prime->binary[i]);
 		Expression(exp);
 		
-		if(i == 0) printf("├%s┬%s┬%s┤\n",line2[0],line2[1],line2[2]);
-		else printf("├%s┼%s┼%s┤\n",line2[0],line2[1],line2[2]);
+		if(i == 0) printf("├%s┬%s┤\n",line2[0],line2[1]);
+		else printf("├%s┼%s┤\n",line2[0],line2[1]);
 		
-		printf("│ %-*s │ %-*s │ " , width[0] , exp , width[1] , prime->binary[i]);
+		printf("│ %-*s │ " , width[0] , exp);
 		for(int j = 0; j < min_count; j++)
 			printf("%-5s" , arr[i][Minterms[j]]);
 		printf(" │\n");
 		
 		free(exp);
 	}
-	printf("╚%s┴%s┴%s╝\n",line2[0],line2[1],line2[2]);
+	printf("╚%s┴%s╝\n",line2[0],line2[1]);
 }
