cec11302b14dad9776bde41ee6b3622e652601db
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-21 22:11:14 +0530

    Update essential-implicants.c
    a working expression transformer for type string_arr

--- source/essential-implicants.c
+++ source/essential-implicants.c
@@ -2,9 +2,31 @@
 #include<string.h>
 #include<stdlib.h>
 #include "quine.h" // quine struture
+#include "string_array.h" // string_arr struture
 #include "helper.h"
 
 //check for improvemnt
+
+int expression(string_arr *binary){
+	for(int index = 0; index < binary->size; index++){
+
+		size_t len = (2*strlen(binary->arr[index])) + 1;
+		char *str = malloc(len);
+		if(!str) return 0;
+
+		int count = 0;
+		for(int i = 0; binary->arr[index][i] != '\0'; i++){
+			char var = 'A'+i;
+			if(binary->arr[index][i] == '0') { str[count++] = var; str[count++] = '\''; }
+			else if(binary->arr[index][i] == '1') str[count++] = var;
+			else continue;
+		}
+		str[count] = '\0';
+		free(binary->arr[index]);
+		binary->arr[index] = str;
+	}
+	return 1;
+}
 
 static int isexist(char **arr, const char item[], int size) {
     for (int i = 0; i < size; i++) {
@@ -13,25 +35,6 @@ static int isexist(char **arr, const char item[], int size) {
     }
     return 0;
 }
-
-static int add_string(char ***str, int *capacity, const char item[], int *count) {
-
-    if (*count >= *capacity) {
-        *capacity += 5;
-        char **temp = realloc(*str, (*capacity) * sizeof(*temp));
-        if (!temp) return 0;
-        *str = temp;
-    }
-
-    size_t len = (2*strlen(item)) + 1;
-    (*str)[*count] = malloc(len);
-    if (!(*str)[*count]) return 0;
-
-    strcpy((*str)[*count], item);
-    (*count)++;
-    return 1;
-}
-
 
 void essential_implicants(quine *prime , char arr[][100][6] , int min_terms[] , int min_count, char result[100] ,int size){
 
@@ -46,8 +49,7 @@ void essential_implicants(quine *prime , char arr[][100][6] , int min_terms[] , 
 			strcpy(arr[i][prime->minterms[i][j]] , " X");
 
 	//Finding the essential implicant by finding column(minterm) with only one 'X' and the prime implecant that covers it
-	char **str = NULL;
-	int count = 0 , capacity = 0;
+	string_arr essential = {NULL , 0 , 0};
 
 	for(int j = 0; j < min_count; j++){
 
@@ -62,25 +64,30 @@ void essential_implicants(quine *prime , char arr[][100][6] , int min_terms[] , 
 			strcpy(arr[index][min_terms[j]] , "(X)" );
 
 			// checking duplicates
-			int check = isexist(str, prime->binary[index] , count);
+			int check = isexist(essential.arr, prime->binary[index] , essential.size);
 			if(check == 1) continue;
 
-			int pass = add_string(&str , &capacity , prime->binary[index] , &count);
+			int pass = add_string(&essential , prime->binary[index]);
 			if(pass == 0){
 				printf("\nERROR: Binary copying fail | Low Memory | essential-implicants\n");
-				free_pointer_array(str , count);
+				free_string_array(&essential);
 				exit(0);
 			}
 		}
 	}
 
-	int offset = 0;
-	for(int i = 0; i < count; i++){
-		Expression(str[i]);
-		int written = snprintf(result+offset , size-offset , i ? "+ %s" : "%s" , str[i]);
-		offset += written;
+	int check = expression(&essential);
+	if(check == 0){
+		printf("\nERROR: expression transform fail | Low Memory | essential-implicants\n");
+		free_string_array(&essential);
+		exit(0);
 	}
 
+	int offset = 0;
+	for(int i = 0; i < essential.size; i++){
+		int written = snprintf(result+offset , size-offset , i ? "+ %s" : "%s" , essential.arr[i]);
+		offset += written;
+	}
 	//free memory
-	free_pointer_array(str , count);
+	free_string_array(&essential);
 }
