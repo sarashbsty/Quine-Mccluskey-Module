aa18e71170437e93df525ab3cc9799b5017b7ccd
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-21 22:10:35 +0530

    Update Expression.c

--- helper_source/Expression.c
+++ helper_source/Expression.c
@@ -5,22 +5,24 @@
 void Expression(char binary[]){
 	char *str = NULL;
 	int cap = 0 , count = 0;
-	
+
 	for(int i = 0; binary[i] != '\0'; i++){
-		
+
 		if(count >= cap-2){
 			cap += 5;
 			char *temp = realloc(str , cap*sizeof(*temp));
 			if(temp == NULL) { printf("ERROR : Low memory   code : Expression\n"); exit(0); }
 			str = temp;
 		}
-		
+
 		char var = 'A'+i;
 		if(binary[i] == '0') { str[count++] = var; str[count++] = '\''; }
 		else if(binary[i] == '1') str[count++] = var;
-		else continue;			
+		else continue;
 	}
 	str[count] = '\0';
 	strcpy(binary,str);
 	free(str);
 }
+
+
