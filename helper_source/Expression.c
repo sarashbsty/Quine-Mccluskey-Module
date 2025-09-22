3985b346aa5f77c64b0b43ede1aa621fe765f912
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-22 09:31:01 +0530

    Update Expression.c
    stored a different method for future use

--- helper_source/Expression.c
+++ helper_source/Expression.c
@@ -25,4 +25,27 @@ void Expression(char binary[]){
 	free(str);
 }
 
+/*
+int expression(string_arr *binary){
+	for(int index = 0; index < binary->size; index++){
+
+		size_t len = (2*strlen(binary->arr[index])) + 1;
+		char *str = malloc(len);
+		if(!str) return 0;
+
+		int count = 0;
+		for(int i = 0; binary->arr[index][i] != '\0'; i++){
+			char var = 'A'+i;
+			if(binary->arr[index][i] == '0') { str[count++] = var; str[count++] = '\''; }
+			else if(binary->arr[index][i] == '1') str[count++] = var;
+			else continue;
+		}
+		str[count] = '\0';
+		free(binary->arr[index]);
+		binary->arr[index] = str;
+	}
+	return 1;
+}
+*/
+
 
