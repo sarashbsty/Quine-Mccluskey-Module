e287b8b9de875acc764af6cfee6d7ba5944e59bb
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-24 21:19:03 +0530

    Update array-to-string.c
    improved the memory allocation strategy

--- helper_source/array-to-string.c
+++ helper_source/array-to-string.c
@@ -2,27 +2,28 @@
 #include "helper.h"
 
 char* array_to_string(const int *arr , const int arr_size ,const char *format){
-	int capacity = 16;
-	int offset = 0;
-	char *str = malloc(capacity * sizeof(*str));
+
+	if (arr_size == 0) {
+        char *str = malloc(1);
+        if (str) str[0] = '\0';
+        return str;
+    }
+
+	//finding requied size
+	int size = 0;
+	for(int i = 0; i < arr_size; i++){
+		int needed = snprintf(NULL , 0 , (i==0) ? "%d" : format , arr[i]);
+		size += needed;
+	}
+
+	//allocate
+	char *str = malloc((size+1) * sizeof(*str));
 	if(!str) return NULL;
 
+	//write
+	int offset = 0;
 	for(int i = 0; i < arr_size; i++){
-
-		// geting required size
-		int needed = snprintf(NULL , 0 , (i==0) ? "%d" : format , arr[i]);
-		int size = offset + needed + 1;
-
-		//reallocate
-		if(size > capacity){
-			while(capacity < size) capacity *= 2;
-			char *temp = realloc(str , capacity * sizeof(*temp));
-			if(!temp) { free(str); return NULL; }
-			str = temp;
-		}
-
-		//Write
-		int written = snprintf(str+offset , capacity-offset , (i==0) ? "%d" : format , arr[i]);
+		int written = snprintf(str+offset , (size+1)-offset , (i==0) ? "%d" : format , arr[i]);
 		offset += written;
 	}
 	return str;
