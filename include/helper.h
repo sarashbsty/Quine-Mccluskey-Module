d4515fa912403d187f0aa59fe7c6584c865bd933
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-10-01 13:05:36 +0530

    Update helper.h

--- include/helper.h
+++ include/helper.h
@@ -32,7 +32,7 @@ static inline void free_2d_pointer(char **arr, int size){
 static inline void free_3d_pointer(char ***arr, int row, int col){
     if(arr == NULL) return;
     for(int i = 0; i < row; i++)
-        free_2d_pointer(arr[i], col);
+        free_2d_pointer((char**)arr[i], col);
     free(arr);
 }
 
