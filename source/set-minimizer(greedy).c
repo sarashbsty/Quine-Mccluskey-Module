35bc7921c68d7ce3b3e33c6389944edbc0b1c532
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2026-01-17 02:14:50 +0530

    Update set-minimizer(greedy).c
    minor changes to loop struture and changed the method of 'deleting' covered minterms

--- source/set-minimizer(greedy).c
+++ source/set-minimizer(greedy).c
@@ -13,28 +13,50 @@ static int isexist(int *arr , int size, int item){
 	return -1;
 }
 
+static int get_no_of_common_terms(int arr1[] , int size1 , int arr2[] , int size2) {
+	int common = 0;
+	for(int i = 0; i < size1; i++){
+		for(int j = 0; j < size2; j++) {
+			if(arr1[i] == arr2[j]){
+				common++;
+				break;
+			}
+		}
+	}
+	return common;
+}
+
+static void delete_at(int *arr, int *size, int pos){
+    if (!arr || !size) return;
+    if (pos < 0 || pos >= *size) return;
+
+    for (int i = pos; i < (*size) - 1; i++)
+        arr[i] = arr[i + 1];
+
+    (*size)--;
+}
+
+
 int set_minimizer(quine *prime , int *minterm_uncovered , int uncovered){
 
-	int covered = 0, covers;
-	do{
-		covers = 0;
-		int pos = -1;
+	while(1){
+		int covers = 0, pos = -1;
+
 		//determines the prime-implicant which covers most minterms in every iteration
 		for(int i = 0; i < prime->count; i++){
 			if(prime->minimal[i] == 1) continue;
-			int new_covers = 0;
-			for(int j = 0; j < uncovered; j++){
-				if(minterm_uncovered[j] == -1) continue;
-				int check = isexist(prime->minterms[i] , prime->mintermCount[i] , minterm_uncovered[j]);
-				if(check != -1) new_covers++;
-			}
+			int new_covers = get_no_of_common_terms(prime->minterms[i] , prime->mintermCount[i] , minterm_uncovered , uncovered);
 			if(new_covers > covers){
 				pos = i;
 				covers = new_covers;
 			}
 		}
 
-		if(pos != -1){
+		if(covers == 0) break;
+ 		else{
+
+			//marking the prime implicant as minimal cover
+			prime->minimal[pos] = 1;
 
 			//Binary to expression
 			char *exp = Expression(prime->binary[pos]);
@@ -42,22 +64,20 @@ int set_minimizer(quine *prime , int *minterm_uncovered , int uncovered){
 			printf("\nSelected %s for minterms : ",exp);
 			free(exp);
 
-			//remove covered minterm
-			for(int j = 0; j < uncovered; j++){
-				if(minterm_uncovered[j] == -1) continue;
-				int check = isexist(prime->minterms[pos] , prime->mintermCount[pos] , minterm_uncovered[j]);
+			//remove covered minterms from the array
+			int idx = 0;
+			while(idx < uncovered){
+				int check = isexist(prime->minterms[pos] , prime->mintermCount[pos] , minterm_uncovered[idx]);
 				if(check != -1){
-					printf("%d ",minterm_uncovered[j]);
-					covered++;
-					minterm_uncovered[j] = -1;
+					printf("%d ",minterm_uncovered[idx]);
+					delete_at(minterm_uncovered, &uncovered, idx);
 				}
+				else idx++;
 			}
-			//marking the prime implicant as minimal cover
-			prime->minimal[pos] = 1;
 		}
-	}while(covers != 0);
+	}
 
-	if(covered == uncovered) return 1;
+	if(uncovered == 0) return 1;
 	else return 0;
 }
 
