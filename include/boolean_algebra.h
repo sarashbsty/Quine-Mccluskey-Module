70c18b04c1bf7a40009bfbcc52b3181a57e77375
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2026-02-14 16:30:18 +0530

    Update boolean_algebra.h
    slight name change in variable

--- include/boolean_algebra.h
+++ include/boolean_algebra.h
@@ -2,4 +2,4 @@
 
 int isSubset(char* A, char* B);
 int absorp(char **termsArr, int count);
-int distributive(char** newTermsArr, char **TermsArr , int count , char *term , int term_size);
+int distributive(char*** returnPtr, char **TermsArr , int count , char *term , int term_size);
