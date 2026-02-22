98566f6ebb91edde7cc3e1e23bf966b2cca62004
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2026-02-22 10:09:06 +0530

    Seperated the whole display section
    Seperaed the whole display section from main into a function

---
+++ include/display_qm.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void display_qm(const char *input);
---
+++ source/qm_display.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parse_input.h"
+#include "qmMinimizer.h"
+#include "memory.h"
+
+void display_qm(const char *input)
+{
+	if(!input) puts("Failed to read input");
+
+	int var, *minterms , minCount , *dontCare, dontCareCount;
+
+	int error = parse_input_json(input, &var, &minterms, &minCount, &dontCare, &dontCareCount);
+
+	if(error == 1)	puts("Parsing input failed due to invalid json");
+
+	else if(error == 2) puts("Some input variable not Found");
+
+	else if(error == 3) puts("memory Allocation fail");
+
+	else if(error == 4) puts("Invalid Datatypes Found");
+
+	int *tmp = realloc(minterms, (minCount + dontCareCount) * sizeof(*tmp));
+	if(!tmp) puts("minterm input reallocation Failed");
+	minterms = tmp;
+
+	//inserting dontcare elements at the end of minterms
+	for(int i = 0; i < dontCareCount; i++)
+		minterms[minCount + i] = dontCare[i];
+
+	//show minterms
+	 printf("%d Min terms: ", minCount);
+    for (int i = 0; i < minCount; i++)
+        printf("%d ", minterms[i]);
+
+	//show dontCares
+    printf("\n%d Dont care: ", dontCareCount);
+    for (int i = 0; i < dontCareCount; i++)
+        printf("%d ", minterms[minCount + i]);
+	printf("\n");
+
+	//call minimizer
+	qmData data = qmMinimizer(minterms, minCount, dontCareCount, var);
+	if(data.error) puts(data.errorMsg);
+
+	//Display Portion
+	for(int i = 0 ; i < data.tablesCount; i++){
+		printf("\nTABLE #%d",i+1);
+		displayTable(&data.tables[i], var);
+	}
+
+	printf("\nPrime Implicants:");
+	displayPi(data.prime , data.primeCount);
+
+	printf("\nPrime Implicant Chart:");
+	displayPiChart(data.prime, data.primeCount, data.piChart, minterms, minCount);
+	if(data.essential){
+		printf("Essential Implicants: ");
+		for(int i = 0; i < data.essentialCount; i++)
+			printf("%s ", data.essential[i]);
+		printf("\n");
+	}
+
+	if(data.uncoveredCount)
+	{
+		if(data.essentialCount)
+		{
+			printf("\nUncovered minterms Prime Implicant Chart:");
+			displayPiChart(data.prime, data.noEssentialPrimeCount, data.piChart, data.uncoveredTerms, data.uncoveredCount);
+			printf("Essential Expressions Removed.\n");
+		}
+
+		if(data.newUncoveredCount)
+		{
+			printf("\nMinterm : ");
+			for(int i = 0; i < data.uncoveredCount; i++){
+				int exist = 0;
+				for(int j = 0; j < data.newUncoveredCount; j++){
+					if(data.uncoveredTerms[i] == data.newUncoveredTerms[j])
+					{ exist = 1; break; }
+				}
+				if(exist == 0) printf("%d ",data.uncoveredTerms[i]);
+			}
+			printf("Removed through Column Domination");
+
+			displayPiChart(data.prime, data.noEssentialPrimeCount,  data.piChart, data.newUncoveredTerms, data.newUncoveredCount);
+		}
+
+		printf("\nlet,\n");
+		for(int i = 0; i < data.noEssentialPrimeCount; i++)
+			printf("  P%d = %s\n", i+1, data.prime[i].expression);
+
+		printf("\nSET Representation:\n");
+		for(int i = 0; i < data.setCount; i++)
+		{
+			if(data.newUncoveredTerms) printf("PI(%d) = ",data.newUncoveredTerms[i]);
+			else printf("PI(%d) = ",data.uncoveredTerms[i]);
+			for(int j = 0; data.set[i][j]; j++){
+				char ch = data.set[i][j];
+				printf(j == 0 ? "P%d" : " + P%d", 1+ch-'A');
+			}
+			printf("\n");
+		}
+
+		if(data.petrick)
+		{
+			printf("\nBy law of Distribution and absorption,\n");
+			for(int i = 0; i < data.petrick->processCount; i++)
+				printf("\n%s\n",data.petrick->process[i]);
+
+			printf("\n\nMinimum literal SOP Terms: ");
+			for(int i = 0; i < data.petrick->SOP_count; i++)
+				printf("%s ",data.petrick->SOP_terms[i]);
+
+			printf("\n\n\nPossible Combinations and Cost:\n\n");
+			for(int i = 0; i < data.petrick->SOP_count; i++)
+			{
+				printf("%d. ",i+1);
+
+				char **terms = data.petrick->combinations[i].terms;
+				int termsCount = data.petrick->combinations[i].termsCount;
+
+				for(int j = 0; j < termsCount; j++)
+					printf( j == 0 ? "%s" : " + %s" , terms[j]);
+
+				printf(" \t\t (%d)\n", data.petrick->combinations[i].cost);
+			}
+
+			printf("\nChosen #%d Combination.\n",1+data.petrick->minCostIdx);
+		}
+	}
+	else printf("\nAll Minterms Covered By Essential Implicants.\n");
+
+	printf("\nResult : Y(");
+	for(int i = 0; i < var; i++)
+		printf( i==0 ? "%c" : ",%c", 'A'+i);
+	printf(") = ");
+
+	for(int i = 0; i < data.resultCount; i++)
+		printf( i == 0 ? "%s" : " + %s", data.result[i]);
+	printf("\n");
+
+	destroyQmData(&data);
+
+	free(minterms);
+	free(dontCare);
+}
+
+
+
+
+
