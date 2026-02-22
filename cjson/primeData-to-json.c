276cf3da164aa78621da24c258de9ea79f85ec79
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2026-02-22 16:51:22 +0530

    Update primeData-to-json.c
    changes made so that empty prime-implciant array can be passed

--- cjson/primeData-to-json.c
+++ cjson/primeData-to-json.c
@@ -6,37 +6,36 @@
 
 cJSON *primeData_to_json(const primeData *prime, int primeCount)
 {
-	if(!prime || !primeCount) return NULL;
-
 	cJSON *primeArr = cJSON_CreateArray();
 	if(!primeArr) goto FAIL;
 
-	for(int i = 0; i < primeCount; i++)
-	{
-		cJSON *row = cJSON_CreateObject();
-		if(!row) goto FAIL;
+	if(prime){
+		for(int i = 0; i < primeCount; i++)
+		{
+			cJSON *row = cJSON_CreateObject();
+			if(!row) goto FAIL;
+			cJSON_AddItemToArray(primeArr, row);
 
-		// binary string
-		cJSON_AddStringToObject(row, "binary", prime[i].binary);
+			// binary string
+			cJSON_AddStringToObject(row, "binary", prime[i].binary);
 
-		// expression string
-		cJSON_AddStringToObject(row, "expression", prime[i].expression);
+			// expression string
+			cJSON_AddStringToObject(row, "expression", prime[i].expression);
 
-		// minterms array
-		cJSON *minterms_arr = cJSON_AddArrayToObject(row, "minterms");
-		if(!minterms_arr) goto FAIL;
+			// minterms array
+			cJSON *minterms_arr = cJSON_AddArrayToObject(row, "minterms");
+			if(!minterms_arr) goto FAIL;
 
-		//add minterms to mintermArr
-		for (int j = 0; j < prime[i].mintermCount; j++)
-			cJSON_AddItemToArray(minterms_arr, cJSON_CreateNumber(prime[i].minterms[j]));
+			//add minterms to mintermArr
+			for (int j = 0; j < prime[i].mintermCount; j++)
+				cJSON_AddItemToArray(minterms_arr, cJSON_CreateNumber(prime[i].minterms[j]));
 
-		//isEssential
-		cJSON_AddBoolToObject(row , "isEssential" , prime[i].isEssential);
+			//isEssential
+			cJSON_AddBoolToObject(row , "isEssential" , prime[i].isEssential);
 
-		//cost
-		cJSON_AddNumberToObject(row , "cost" , prime[i].cost);
-
-		cJSON_AddItemToArray(primeArr, row);
+			//cost
+			cJSON_AddNumberToObject(row , "cost" , prime[i].cost);
+		}
 	}
 
 	return primeArr;
